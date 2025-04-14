16a21243872faeba41b5ab76d26b8315afb5065a
Aatmik-06 <adi120kush@gmail.com>
2025-04-15 00:07:59 +0530

    updated

--- app/components1/ui/form.tsx
+++ app/components1/ui/form.tsx
@@ -11,7 +11,7 @@ import {
 } from "react-hook-form"
 
 import { cn } from "@/lib/utils"
-import { Label } from "@/components1/ui/label"
+import { Label } from "./label"
 
 const Form = FormProvider
 
